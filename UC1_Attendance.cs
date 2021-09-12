4663416c229013761330458279fc729aba1b55bb
umamahale <89057939+umamahale@users.noreply.github.com>
2021-09-12 08:52:10 -0400

    Add files via upload

---
+++ UC1_Attendance.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmpWageComp
+{
+    class UC1_Attendance
+    {
+        //UC1
+        public static void GetAttendance()
+        {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+                Console.WriteLine("Employee is present");
+            else
+
+                Console.WriteLine("Employee is absent");
+
+        }
+
+
+    }
+}
